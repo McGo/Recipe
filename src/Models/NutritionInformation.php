ec4b335e17809aa0f200d8c83a9a39c74bfe2336
Mirko Haaser <mirko@topred.media>
2023-02-17 19:15:42 +0100

    ..

--- src/Models/NutritionInformation.php
+++ src/Models/NutritionInformation.php
@@ -9,6 +9,15 @@ class NutritionInformation extends Model
     public $timestamps = false;
     protected $table = 'mcgo_recipe_nutrition_informations';
     protected $guarded = [];
+    protected $casts = [
+        'values_per_g' => 'float',
+        'calories' => 'float',
+        'fett_g' => 'float',
+        'kohlenhydrate_g' => 'float',
+        'zucker_g' => 'float',
+        'eiweiss_g' => 'float',
+        'ballaststoffe_g' => 'float'
+    ];
 
     public function nutriable()
     {
